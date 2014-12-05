82b44b19f577762cf572751c4a2f9a7733588a0d
Ioannis Tziakos <ioannist@enthought.com>
2014-12-05 23:20:25 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
-#----------------------------------------------------------------------------
-#
-#  Copyright (c) 2014, Enthought, Inc.
-#  All rights reserved.
-#
-#  This software is provided without warranty under the terms of the BSD
-#  license included in /LICENSE.txt and may be redistributed only
-#  under the conditions described in the aforementioned license.  The license
-#  is also available online at http://www.enthought.com/licenses/BSD.txt
-#
-#  Thanks for using Enthought open source!
-#
-#----------------------------------------------------------------------------
-
 from setuptools import setup, find_packages
 
 
@@ -20,7 +6,25 @@ setup(
     version='1.0.0',
     author='Enthought, Inc',
     author_email='info@enthought.com',
+    maintainer='Ioannis Tziakos',
+    maintainer_email='info@enthought.com',
+
     url='https://github.com/enthought/trait-documenter',
     description='Autodoc extention for documenting traits',
     long_description=open('README.rst').read(),
-    packages=find_packages())
+    license="BSD",
+    classifiers=[
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Sphinx :: Extension",
+        "Topic :: Documentation :: Sphinx",
+    ],
+    packages=['trait_documenter', 'trait_documenter.test'])
