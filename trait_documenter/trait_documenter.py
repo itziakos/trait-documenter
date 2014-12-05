0d2f10dc985d2356d885fdf9fee9e441be595261
Ioannis Tziakos <ioannist@enthought.com>
2014-12-05 23:20:10 +0000

    update copyright statement

--- trait_documenter/trait_documenter.py
+++ trait_documenter/trait_documenter.py
@@ -1,10 +1,16 @@
-"""
-    A Trait Documenter
-    (Subclassed from the autodoc ClassLevelDocumenter)
-
-    :copyright: Copyright 2014 by Enthought, Inc
-
-"""
+#----------------------------------------------------------------------------
+#
+#  Copyright (c) 2014, Enthought, Inc.
+#  All rights reserved.
+#
+#  This software is provided without warranty under the terms of the BSD
+#  license included in /LICENSE.txt and may be redistributed only
+#  under the conditions described in the aforementioned license.  The license
+#  is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+#  Thanks for using Enthought open source!
+#
+#----------------------------------------------------------------------------
 import ast
 import traceback
 import sys
